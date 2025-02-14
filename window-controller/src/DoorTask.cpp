0f93c556f52393a61b7eb08fb1b98a22a24e498c
miriamsonaglia <miriamsonaglia@gmail.com>
2025-02-14 11:06:24 +0100

    fixed bug

--- window-controller/src/DoorTask.cpp
+++ window-controller/src/DoorTask.cpp
@@ -1,10 +1,12 @@
 #include "../lib/Tasks/DoorTask.h"
 #include "../lib/Scheduling/SharedData.h"
+#include <Arduino.h>
 
 extern struct SharedData share_data;
 
 DoorTask::DoorTask(int pin){
     DoorTask::door = new Door(pin);
+    Serial.begin(9600);
 }
 
 void DoorTask::init(int period){
@@ -14,10 +16,10 @@ void DoorTask::init(int period){
 
 void DoorTask::tick(){
 
-    status = (State)updateStatus();
+    status = updateStatus();
 
     if(status==READ_FROM_SYSTEM){
-        door->setAngle(0);
+        door->setAngle(150);
     }
     else if(status==READ_FROM_POT){
         door->setAngle(60);
@@ -29,10 +31,12 @@ void DoorTask::tick(){
 
 int DoorTask::updateStatus(){
     if(status!=NOT_READING){
+        Serial.println(share_data.switch_mode);
         if(share_data.switch_mode){
             share_data.switch_mode = 0;
             return (status==READ_FROM_SYSTEM) ? READ_FROM_POT : READ_FROM_SYSTEM;
         }
+        return status;
     }
     return NOT_READING;
 }
