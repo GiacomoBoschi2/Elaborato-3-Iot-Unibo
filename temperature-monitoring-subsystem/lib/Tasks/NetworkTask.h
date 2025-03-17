c75facf8c1d970fe25c6b0ca5e42a2539c5e651d
Giacomo Boschi <giacomo.boschi7@studio.unibo.it>
2025-03-17 16:39:13 +0000

    Defined network task

--- temperature-monitoring-subsystem/lib/Tasks/NetworkTask.h
+++ temperature-monitoring-subsystem/lib/Tasks/NetworkTask.h
@@ -1,1 +1,17 @@
-#define __
+#ifndef __NETWORKTASK__
+#define __NETWORKTASK__
+
+#include "Task.h"
+#include <Ethernet.h>   
+
+
+class NetworkTask: public Task{
+    public:
+        NetworkTask(IPAddress address);
+        void init(int period);
+        void tick();
+    private:
+        IPAddress address;
+};
+
+#endif
